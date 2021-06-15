264435836dfe7c5011ce75e41dae7eabecc0e18e
rdc6087940 <84650497+rdc6087940@users.noreply.github.com>
2021-06-15 09:28:16 +0900

    Create practice_05-튜플.py

---
+++ practice_05-튜플.py
@@ -0,0 +1,17 @@
+# 변경하거나 수정할 수 없지만 리스트보다 빠르게 사용가능하다.
+
+# 튜플의 형식
+menu = ("돈까스", "치즈가스")
+print(menu[0])
+print(menu[1])
+
+# 이렇게 나눠서 선언하는것을
+# name = "김종국"
+# age = 20
+# hobby = "코딩"
+# print(name, age, hobby)
+
+# 튜플을 사용하면 이와같이 이용할 수 있다.
+
+(name, age, hobby) = ("김종국", 20, "코딩")
+print(name, age, hobby)
