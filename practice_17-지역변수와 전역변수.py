2b9e3682ae34f6875ab4ca2e05c8b13a91404146
rdc6087940 <84650497+rdc6087940@users.noreply.github.com>
2021-06-18 09:29:24 +0900

    Create practice_17-지역변수와 전역변수.py

---
+++ practice_17-지역변수와 전역변수.py
@@ -0,0 +1,20 @@
+gun = 10
+
+def checkpoint(soldiers):
+    global gun
+    gun = gun-soldiers
+    print("[함수 내] 남은 총 : {0}".format(gun))
+
+def checkpoint_return(gun, soldiers):
+    gun = gun - soldiers
+    print("[함수 내] 남은 총 : {0}".format(gun))
+    return gun
+
+
+print("전체 총 : {0}".format(gun))
+
+# checkpoint(2)
+# print("남은 총 : {0}".format(gun))
+
+gun = checkpoint_return(gun, 2)
+print("남은 총 : {0}".format(gun))
